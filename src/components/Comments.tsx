ab27977eed295092f235afecdf1a14ce056e77ed
Kael Paraguassu <kaelparaguassu@hotmail.com>
2022-08-04 22:02:00 -0300

    estilizacao de comentarios inicial

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,11 +1,16 @@
 import {useState, useEffect} from 'react';
-// import { Divider, Avatar, Grid, Paper } from "@mui/material";
+import { Divider, Avatar, Grid, Paper, Button } from "@mui/material";
 import { Container, List, Typography } from "@mui/material";
 
 const imgLink = "https://images.pexels.com/photos/1681010/pexels-photo-1681010.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260";
 
 export default function Comments(props: any) {
-  const [comments, setComments] = useState([]);
+  const [comments, setComments] = useState([{postId: 0, id: 0, name: "", email: "", body: ""}]);
+  const [openComments, setOpenComments] = useState(true);
+
+  const handleOpenComments = () => {
+    setOpenComments(!openComments);
+  };
 
   useEffect(() => {
     fetch(`https://jsonplaceholder.typicode.com/posts/${props.post}/comments`)
@@ -14,10 +19,29 @@ export default function Comments(props: any) {
   })
 
   return (
-    <Container sx={{ padding: 14 }}>
-      <Typography variant="h1">Comments {props.post} {comments.length} </Typography>
-      <List>
-        {comments.map((comment) => <Typography color={'white'}>{comment.name}</Typography>)}
+    <Container sx={{ padding: 5 }}>
+      <Button>
+        <Typography variant="h6" onClick={handleOpenComments}>
+          Comments ({comments.length}) 
+        </Typography>
+      </Button>
+      <List hidden={openComments}>
+        {comments.map((comment) => (
+          <>
+            <Grid container wrap="nowrap" spacing={2}>
+              <Grid item>
+                <Avatar alt="Remy Sharp" src={imgLink} />
+              </Grid>
+              <Grid justifyContent="left" item xs zeroMinWidth>
+                <h4 style={{ margin: 0, textAlign: "left" }}>{comment.name}</h4>
+                <p style={{ textAlign: "left" }}>
+                  {comment.body}
+                </p>
+              </Grid>
+            </Grid>
+            <Divider variant="fullWidth" style={{ margin: "30px 0" }} />
+          </>
+        ))}
       </List>
     </Container>
   );
