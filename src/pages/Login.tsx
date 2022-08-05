51d32e51a17dc9754f8507f5297e822025a13bf7
Kael Paraguassu <kaelparaguassu@hotmail.com>
2022-08-05 20:17:50 -0300

    substituicao de página login por template mui

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,63 +1,98 @@
-import FingerprintIcon from "@mui/icons-material/Fingerprint";
-import HomeIcon from "@mui/icons-material/Home";
-import { Typography } from "@mui/material";
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
-import Container from "@mui/material/Container";
+import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { IconButton } from "@mui/material";
+import KeyboardReturnIcon from "@mui/icons-material/KeyboardReturn";
 
-const Login = () => {
+export default function Login() {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
 
-    const [name, setName] = useState('');
-    const [password, setPassword] = useState('');
-
-    const handleChangeName = (event: any) => {
-        setName(event.target.value);
-    }
-  
-
-    return (
-
-        <Container>
-
-            <TextField 
-                id="outlined-basic" 
-                label="apelido ou email de usuário" 
-                variant="outlined"
-                value={name}
-                onChange={handleChangeName}
-            />
-
-            <TextField 
-                label="Senha" 
-                type="password" 
-                value={password}
-                onChange={(event) => setPassword(event.target.value)}
-            />
-
-            <Button variant="contained" endIcon={<HomeIcon />} >
-                <Link to={"/"}>
-                    <Typography>Voltar</Typography>
-                </Link>
-            </Button>
-
-
-            <Button 
-                variant='contained'
-                disabled={!(!!name && !!password)}
-                endIcon={<FingerprintIcon />}
-            >
-
-                <Link to={`/`}>
-                    <Typography>Entrar no chat</Typography>
-                </Link>
-
-            </Button>
-
-        </Container>
-
-    );
-};
-
-export default Login;
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Link href="/">
+          <IconButton aria-label="delete">
+            <KeyboardReturnIcon />
+          </IconButton>
+        </Link>
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Login
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Endereço de Email"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Senha"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Lembre-se de mim"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Entrar
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Esqueceu sua senha?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="/cadastro" variant="body2">
+                {"Cadastre-se"}
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
+  );
+}
